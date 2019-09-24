a7f92b1aef9d872a36ae269b7653a5fba6bd0897
Andrew Baker <andrew@chronalstudios.com>
2019-09-24 07:22:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install flatpak
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.teamspeak.TeamSpeak
